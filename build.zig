63123afadfa7c8799c4dbe2ae62d89992dfa0baa
Kent Smith <kentsmith@gmail.com>
2025-04-10 17:18:09 -0700

    Add test coverage
    Stolen from zigtools/zls

--- build.zig
+++ build.zig
@@ -5,10 +5,9 @@ const std = @import("std");
 // runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("tmz", .{ .root_source_file = b.path("src/tmz.zig") });
+    const coverage = b.option(bool, "coverage", "Generate a coverage report with kcov") orelse false;
 
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/tmz.zig"),
@@ -28,8 +27,31 @@ pub fn build(b: *std.Build) void {
         .root_module = lib_mod,
     });
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const run_tests = b.addRunArtifact(unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    test_step.dependOn(&run_tests.step);
+
+    if (coverage) {
+        const kcov_bin = b.findProgram(&.{"kcov"}, &.{}) catch "kcov";
+        const merge_step = std.Build.Step.Run.create(b, "merge coverage");
+        merge_step.addArgs(&.{ kcov_bin, "--merge" });
+        merge_step.rename_step_with_output_arg = false;
+        const merged_coverage_output = merge_step.addOutputFileArg(".");
+
+        run_tests.setName(b.fmt("{s} (collect coverage)", .{run_tests.step.name}));
+        // prepend the kcov exec args
+        const argv = run_tests.argv.toOwnedSlice(b.allocator) catch @panic("OOM");
+        run_tests.addArgs(&.{ kcov_bin, "--collect-only" });
+        run_tests.addPrefixedDirectoryArg("--include-pattern=", b.path("src"));
+        merge_step.addDirectoryArg(run_tests.addOutputFileArg(run_tests.producer.?.name));
+        run_tests.argv.appendSlice(b.allocator, argv) catch @panic("OOM");
+
+        const install_coverage = b.addInstallDirectory(.{
+            .source_dir = merged_coverage_output,
+            .install_dir = .{ .custom = "coverage" },
+            .install_subdir = "",
+        });
+        test_step.dependOn(&install_coverage.step);
+    }
 }
